108cfa54e856f0149dbecda7d1ee44acd241328a
Jonathan <jonathanlukas17116@gmail.com>
2025-11-22 09:57:57 +0700

    supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:ppb_journey_app/screens/events/event_list_screen.dart'; 
+import 'package:ppb_journey_app/screens/events/event_list_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   runApp(const MyApp());
 }
 
@@ -14,9 +22,9 @@ class MyApp extends StatelessWidget {
       title: 'Aplikasi Pendukung Event Trip',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.teal, 
+        primarySwatch: Colors.teal,
         scaffoldBackgroundColor: Colors.white,
-        fontFamily: 'Poppins', 
+        fontFamily: 'Poppins',
       ),
 
       home: const EventListScreen(),
